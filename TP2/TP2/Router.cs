91092a7ed4494e52368f18189b47250cfd80a77e
Sebastien Pellerin <sebi.pellerin@gmail.com>
2016-02-16 09:29:03 -0500

    Initial Commit

---
+++ TP2/TP2/Router.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+  class Router
+  {
+    #region Propriétés
+    /// <summary>
+    /// Mode du router (LS/DV)
+    /// </summary>
+    public static ModeEnum RouterMode { get; set; }
+    #endregion
+
+    #region Constructeur
+    public Router()
+    {
+
+    }
+    #endregion
+
+    #region Méthodes publiques
+
+    #endregion
+
+    #region Méthodes privées
+
+    #endregion
+  }
+}
